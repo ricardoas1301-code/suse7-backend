7c128b83c37ada36950cce1759f0bfeb750022b1
Ricardo Alexandre <ricardoas1301@gmail.com>
2025-12-08 15:38:52 -0300

    Deploy backend Suse7

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(mlRoutes);
 // ---------------------------------------------------------
 // Iniciar servidor
 // ---------------------------------------------------------
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Backend do Suse7 rodando na porta ${PORT}`);
 });
